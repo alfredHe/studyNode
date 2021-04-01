b936ab58d6655d6fdca1a1373d05b1e95a70282a
hefayou <hefayou@sksedu.com>
2021-04-01 18:21:23 +0800

    mysql集成到node项目中

--- mysql-test/index.js
+++ mysql-test/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'onlyhefayou520??',
+    password: '123456',
     port: 3306,
     database: 'myblog'
 })
